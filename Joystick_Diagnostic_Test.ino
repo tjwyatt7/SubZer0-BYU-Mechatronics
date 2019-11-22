c51bf08a7e47bc8a0117edd24123feb0a4d27960
sashford <58056446+sashford@users.noreply.github.com>
2019-11-21 18:50:39 -0700

    Update Joystick_Diagnostic_Test.ino

--- Joystick_Diagnostic_Test.ino
+++ Joystick_Diagnostic_Test.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+#define joy_xPin A1
+#define joy_yPin A2
+
 LiquidCrystal_I2C lcd(0x3F,16,2);
 int xVal = 0;
 int yVal = 0;
@@ -10,6 +13,8 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.begin(16,2);
+  pinMode(joy_xPin, INPUT);
+  pinMode(joy_yPin, INPUT);
 
   lcd.setCursor(0,0);
   lcd.print("Ready");
@@ -19,7 +24,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lcd.print("xVal: " + xVal + "/n");
-  lcd.print("yVal: " + yVal + "/n");
-  lcd.clear();
+  xVal = analogRead(joy_xPin);
+  yVal = analogRead(joy_yPin);
+
+  
+  lcd.print("xVal: " + xVal); // Print out current x-value
+  lcd.print("/nyVal: " + yVal); // Print out current y-value
+  lcd.clear();                       // Clear out extra space
 }
